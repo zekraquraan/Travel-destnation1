401934fa5402eb84808ee8240fe5d3f79c4a6759
zekraquraan <zekraquraan7@gmail.com>
2023-04-04 11:27:27 +0000

    updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
-function Footer(){    return (
-        <><a href="https://www.instagram.com/">Instgram</a><p>Zekra</p>
-        </>    )}
+function Footer() {
+        return (
+                <><a href="https://www.instagram.com/">Instgram</a><p>Zekra</p>
+                </>)
+}
 export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "../Header/Header.js";
 import Tours from "../Tours/Tours.js";
 import Footer from "../Footer/Footer.js";
-const Data2= require("../../Data/db.json");
-function Home (){
-    return(        <>
-<Header/>
- {  Data2.map(i=>{          
-    return(            <Tours name ={i.name} image_url={i.image}/>          )        })}
-<Footer/>
- </>)}
+//const Data1= require("./Data/db.json");
+function Home(props) {
+    const data = props.data;
+    return (
+        <>
+
+
+            <Header />
+
+            {data.map(element => {
+                return (
+                    <Tours name={element.name} image_url={element.image} />
+                )
+            })}
+
+            <Footer />
+
+
+
+        </>
+    )
+}
+
 export default Home;
